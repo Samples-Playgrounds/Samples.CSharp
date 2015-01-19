7aa72dae7bffda62c04443f5b5497ad553e2b4fe
moljac <mcvjetko@holisticware.net>
2015-01-19 23:25:27 +0100

    UsingStaticClassMethods

---
+++ v.6/CSharp.V6/Features/UsingStatic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Note: this is class not namespace
+namespace CSharp.V6.Features
+{
+	class UsingStatic
+	{
+	}
+}
---
+++ v.6/CSharp.V6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.V6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			UsingStaticClass.UsingStaticClassMethodsWithoutClassSpecification();
+
+			return;
+		}
+	}
+}
