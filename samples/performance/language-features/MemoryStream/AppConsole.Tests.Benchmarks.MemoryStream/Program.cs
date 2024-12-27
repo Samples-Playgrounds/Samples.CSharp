b92a17512521924a60d1e43e111919a4590f46a9
moljac <mcvjetko@holisticware.net>
2024-12-27 22:34:26 +0100

    MemoryStream

---
+++ samples/performance/language-features/MemoryStream/AppConsole.Tests.Benchmarks.MemoryStream/Program.cs
@@ -0,0 +1,8 @@
+﻿using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+
+using Holisticware.Library.Snippets.MemoryStream;
+
+Summary summary = BenchmarkRunner.Run<Benchmarks_MemoryStream>();
+
+return;
