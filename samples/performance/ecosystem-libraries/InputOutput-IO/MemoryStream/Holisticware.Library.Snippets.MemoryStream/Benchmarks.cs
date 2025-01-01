a494f49e04c7b185bc0ab17f9f91b2a7823022f1
moljac <mcvjetko@holisticware.net>
2025-01-01 20:57:53 +0100

    MemoryStream

---
+++ samples/performance/ecosystem-libraries/InputOutput-IO/MemoryStream/Holisticware.Library.Snippets.MemoryStream/Benchmarks.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.IO;
+using BenchmarkDotNet.Attributes;
+
+namespace Holisticware.Library.Snippets.MemoryStream;
+
+[MemoryDiagnoser]
+public partial class
+                                        Benchmarks_MemoryStream
+{
+    public static readonly RecyclableMemoryStreamManager rmsm_static = new();
+    
+    [Benchmark]
+    [Arguments("files/email50.csv")]
+    [Arguments("files/Izvjesce-o-provedbi-ZPPI-za-2023.doc")]
+    [Arguments("files/Sazetak-Izvjesca-o-provedbi-ZPPI-za-2023.doc")]
+    [Arguments("files/17818489.zip")]
+    [Arguments("files/WhatsApp Image 2024-11-06 at 11.45.19.jpeg")]
+    [Arguments("files/WhatsApp Image 2024-11-06 at 11.57.27.jpeg")]
+    [Arguments("files/plavi-tjedan.jpg")]
+    public
+        void
+                                        File_doc_to_MemoryStream
+                                        (
+                                            string file
+                                        )
+    {
+        using (FileStream fs = File.OpenRead(file))
+        {
+            System.IO.MemoryStream ms = new ();
+            ms.SetLength(fs.Length);
+            fs.Read(ms.GetBuffer(), 0, (int)fs.Length);
+        }
+        
+        return;
+    }
+    
+    [Benchmark]
+    [Arguments("files/email50.csv")]
+    [Arguments("files/Izvjesce-o-provedbi-ZPPI-za-2023.doc")]
+    [Arguments("files/Sazetak-Izvjesca-o-provedbi-ZPPI-za-2023.doc")]
+    [Arguments("files/17818489.zip")]
+    [Arguments("files/WhatsApp Image 2024-11-06 at 11.45.19.jpeg")]
+    [Arguments("files/WhatsApp Image 2024-11-06 at 11.57.27.jpeg")]
+    [Arguments("files/plavi-tjedan.jpg")]
+    public
+        void
+                                        File_to_static_RecyclableMemoryStreamManager_RecyclableMemoryStream
+                                        (
+                                            string file
+                                        )
+    {
+        RecyclableMemoryStream rms = rmsm_static.GetStream(File.ReadAllBytes(file));
+        
+        return;
+    }
+}
---
+++ samples/performance/ecosystem-libraries/InputOutput-IO/MemoryStream/Holisticware.Library.Snippets.MemoryStream/readme.md
@@ -0,0 +1,20 @@
+# Benchmrks for `MemoryStream`
+
+*   https://github.com/microsoft/Microsoft.IO.RecyclableMemoryStream
+
+*   https://stackoverflow.com/questions/75003773/json-serialize-error-via-recyclablememorystream
+
+*   https://www.infoworld.com/article/2261281/how-to-use-recyclablememorystream-in-net-core.html
+
+*   https://medium.com/@matias.paulo84/recyclablememorystream-vs-memorystream-c4aa341324a9
+
+*   https://github.com/matipaulo/Demos
+
+
+`!=` vs `is not null`
+
+https://www.reddit.com/r/csharp/comments/1gzvel1/null_is_not_null/
+
+is null/is not null does not incur method invocation cost in the event that ==/!= are overridden. 
+
+most likely RyuJIT is going to optimize that pretty heavily.
