30ff19449caa5eeaf0e8ef1906878ea75c4487cd
moljac <mcvjetko@holisticware.net>
2025-02-25 10:26:27 +0100

    Null Checking

---
+++ samples/performance/language-features/Null-Checking-Nullable/Holisticware.Library.Snippets.Nullable/Benchmarks.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.RegularExpressions;
+using BenchmarkDotNet.Attributes;
+
+namespace Holisticware.Library.Snippets.Regex;
+
+[MemoryDiagnoser]
+public partial class
+                                        Benchmarks_Nullable_NullChecks
+{
+    [Benchmark]
+    [Arguments("files/email50.csv")]
+    [Arguments("")]
+    [Arguments(null)]
+    [Arguments(default(string))]
+    public
+        void
+                                        Null_Check_string_is_null
+                                        (
+                                            string file
+                                        )
+    {
+        if (file is null)
+        {
+        }
+        
+        return;
+    }
+    
+    [Benchmark]
+    [Arguments("files/email50.csv")]
+    [Arguments("")]
+    [Arguments(null)]
+    [Arguments(default(string))]
+    public
+        void
+                                        Null_Check_string_operator_equivalence
+                                        (
+                                            string file
+                                        )
+    {
+        if (file == null)
+        {
+        }
+        
+        return;
+    }
+    
+    [Benchmark]
+    [Arguments("files/email50.csv")]
+    [Arguments("")]
+    [Arguments(null)]
+    [Arguments(default(System.IO.MemoryStream))]
+    public
+        void
+                                        Null_Check_string_operator_equivalence
+                                        (
+                                            object file
+                                        )
+    {
+        if (file == null)
+        {
+        }
+        
+        return;
+    }
+    
+}
