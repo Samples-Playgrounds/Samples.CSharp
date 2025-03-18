e409791c4c5be2e31e30e4c00103fb7d684acf75
moljac <mcvjetko@holisticware.net>
2025-03-18 22:58:13 +0100

    Update Benchmarks.cs

--- samples/performance/ecosystem-libraries/InputOutput-IO/FileReading.Text/Holisticware.Library.Snippets.FileReading.Text/Benchmarks.cs
+++ samples/performance/ecosystem-libraries/InputOutput-IO/FileReading.Text/Holisticware.Library.Snippets.FileReading.Text/Benchmarks.cs
@@ -25,7 +25,7 @@ namespace Holisticware.Library.Snippets.ControlStructures.RangeMatching;
 [XmlExporterAttribute.BriefCompressed]
 [XmlExporterAttribute.Full]
 [XmlExporterAttribute.FullCompressed]
-//[Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Declared)]
+[Orderer(SummaryOrderPolicy.FastestToSlowest, MethodOrderPolicy.Declared)]
 [HideColumns("Error", "StdDev")]
 public partial class
     Benchmarks_FileReading_Text
@@ -42,10 +42,10 @@ public partial class
         string
                                         File_ReadAllLines_StringBuilder
                                         (
-                                            string file
+                                            string file_path_test_data
                                         )
     {
-        string[] lines = File.ReadAllLines(file);
+        string[] lines = File.ReadAllLines(file_path_test_data);
         System.Text.StringBuilder stringBuilder = new System.Text.StringBuilder();
 
         foreach (var line in lines)
@@ -70,10 +70,10 @@ public partial class
         string
                                         File_ReadAllLines_ZString
                                         (
-                                            string file
+                                            string file_path_test_data
                                         )
     {
-        string[] lines = File.ReadAllLines(file);
+        string[] lines = File.ReadAllLines(file_path_test_data);
         System.Text.StringBuilder stringBuilder = new System.Text.StringBuilder();
 
         using Cysharp.Text.Utf16ValueStringBuilder sb = Cysharp.Text.ZString.CreateStringBuilder();
@@ -97,10 +97,10 @@ public partial class
         string
                                         File_ReadAllText
                                         (
-                                            string file
+                                            string file_path_test_data
                                         )
     {
-        return System.IO.File.ReadAllText(file);
+        return System.IO.File.ReadAllText(file_path_test_data);
     }
 
     [Benchmark]
@@ -115,10 +115,10 @@ public partial class
         string
                                         StreamReader_ReadLine_StringBuilder
                                         (
-                                            string file
+                                            string file_path_test_data
                                         )
     {
-        using StreamReader sr = new StreamReader(file);
+        using StreamReader sr = new StreamReader(file_path_test_data);
         System.Text.StringBuilder sb = new System.Text.StringBuilder();
 
         while (sr.ReadLine() is { } fileLine)
@@ -141,10 +141,10 @@ public partial class
         string
                                         StreamReader_ReadLine_ZString
                                         (
-                                            string file
+                                            string file_path_test_data
                                         )
     {
-        using StreamReader sr = new StreamReader(file);
+        using StreamReader sr = new StreamReader(file_path_test_data);
         
         using Cysharp.Text.Utf16ValueStringBuilder sb = Cysharp.Text.ZString.CreateStringBuilder();
 
@@ -168,10 +168,10 @@ public partial class
         string
                                         StreamReader_ReadToEnd
                                         (
-                                            string file
+                                            string file_path_test_data
                                         )
     {
-        using System.IO.StreamReader sr = new System.IO.StreamReader(file);
+        using System.IO.StreamReader sr = new System.IO.StreamReader(file_path_test_data);
         
         return sr.ReadToEnd();
     }
@@ -188,10 +188,10 @@ public partial class
         string
                                         StreamReader_ReadBlock_StringBuilder
                                         (
-                                            string file
+                                            string file_path_test_data
                                         )
     {
-        using System.IO.StreamReader sr = new System.IO.StreamReader(file);
+        using System.IO.StreamReader sr = new System.IO.StreamReader(file_path_test_data);
         
         char[] buffer = new char[4096];
         int numberRead;
@@ -216,10 +216,10 @@ public partial class
         string
                                         StreamReader_ReadBlock_ZString
                                         (
-                                            string file
+                                            string file_path_test_data
                                         )
     {
-        using System.IO.StreamReader sr = new System.IO.StreamReader(file);
+        using System.IO.StreamReader sr = new System.IO.StreamReader(file_path_test_data);
         
         char[] buffer = new char[4096];
         int numberRead;
@@ -244,10 +244,10 @@ public partial class
         string
                                         StreamReader_ReadBlock_ArrayPool_StringBuilder
                                         (
-                                            string file
+                                            string file_path_test_data
                                         )
     {
-        using System.IO.StreamReader sr = new System.IO.StreamReader(file);
+        using System.IO.StreamReader sr = new System.IO.StreamReader(file_path_test_data);
         
         char[] buffer = System.Buffers.ArrayPool<char>.Shared.Rent(4096);
         int numberRead;
@@ -272,10 +272,10 @@ public partial class
         string
                                         StreamReader_ReadBlock_ArrayPool_ZString
                                         (
-                                            string file
+                                            string file_path_test_data
                                         )
     {
-        using System.IO.StreamReader sr = new System.IO.StreamReader(file);
+        using System.IO.StreamReader sr = new System.IO.StreamReader(file_path_test_data);
         
         char[] buffer = System.Buffers.ArrayPool<char>.Shared.Rent(4096);
         int numberRead;
@@ -300,10 +300,10 @@ public partial class
         string
                                         StreamReader_ReadBlock_Span_StringBuilder
                                         (
-                                            string file
+                                            string file_path_test_data
                                         )
     {
-        using System.IO.StreamReader sr = new System.IO.StreamReader(file);
+        using System.IO.StreamReader sr = new System.IO.StreamReader(file_path_test_data);
         
         Span<char> buffer = new char[4096].AsSpan();
         int numberRead;
@@ -328,10 +328,10 @@ public partial class
         string
                                         StreamReader_ReadBlock_Span_ZString
                                         (
-                                            string file
+                                            string file_path_test_data
                                         )
     {
-        using System.IO.StreamReader sr = new System.IO.StreamReader(file);
+        using System.IO.StreamReader sr = new System.IO.StreamReader(file_path_test_data);
         
         Span<char> buffer = new char[4096].AsSpan();
         int numberRead;
@@ -357,12 +357,12 @@ public partial class
         string
                                         StreamReader_FileStream_BufferedStream_ReadLine_ZString
                                         (
-                                            string file
+                                            string file_path_test_data
                                         )
     {
         using System.IO.FileStream fs = new System.IO.FileStream
                                                             (
-                                                                file,
+                                                                file_path_test_data,
                                                                 FileMode.Open,
                                                                 FileAccess.Read,
                                                                 FileShare.Read
@@ -393,12 +393,12 @@ public partial class
         string
                                         StreamReader_FileStream_MemoryStream_ReadLine_ZString
                                         (
-                                            string file
+                                            string file_path_test_data
                                         )
     {
         using System.IO.FileStream fs = new System.IO.FileStream
                                                             (
-                                                                file,
+                                                                file_path_test_data,
                                                                 FileMode.Open,
                                                                 FileAccess.Read,
                                                                 FileShare.Read
@@ -422,6 +422,19 @@ public partial class
         Microsoft.IO.RecyclableMemoryStreamManager 
                                         rmsm = new Microsoft.IO.RecyclableMemoryStreamManager();
 
+    [GlobalSetup(Target = nameof(ReadAllTextWithFileUsingStreamRecyclableAndMemoryStreamAndStreamReaderAndReadBlockIntoZString_InDirect))]
+    public
+        void
+                                        Setup_ReadAllTextWithFileUsingStreamRecyclableAndMemoryStreamAndStreamReaderAndReadBlockIntoZString_InDirect
+                                        (
+                                        )
+    {
+        Core.IO.File.ReadAllTextImplementation 
+                = Core.IO.File.ReadAllTextWithFileUsingStreamRecyclableAndMemoryStreamAndStreamReaderAndReadBlockIntoZString;
+        
+        return;
+    }
+    
     [Benchmark]
     [Arguments("test-data/192.txt")]
     [Arguments("test-data/258.txt")]
@@ -432,30 +445,32 @@ public partial class
     [Arguments("test-data/558.50mb.csv")]
     public
         string
-                                        StreamReader_FileStream_RecyclableMemoryStream_ReadLine_ZString
+                                        ReadAllTextWithFileUsingStreamRecyclableAndMemoryStreamAndStreamReaderAndReadBlockIntoZString_InDirect
                                         (
-                                            string file
+                                            string file_path_test_data
                                         )
     {
-        using System.IO.FileStream fs = new System.IO.FileStream
-                                                            (
-                                                                file,
-                                                                FileMode.Open,
-                                                                FileAccess.Read,
-                                                                FileShare.Read
-                                                            );
-        using Microsoft.IO.RecyclableMemoryStream ms = rmsm.GetStream();
-        fs.CopyTo(ms);
-        using System.IO.StreamReader sr = new System.IO.StreamReader(ms);
-        
-        Span<char> buffer = new char[4096].AsSpan();
-        int numberRead;
-        using Cysharp.Text.Utf16ValueStringBuilder sb = Cysharp.Text.ZString.CreateStringBuilder();
-        while ((numberRead = sr.ReadBlock(buffer)) > 0)
-        {
-            sb.Append(buffer[..numberRead]);
-        }
-        
-        return sb.ToString();        
+        return Core.IO.File.ReadAllText(file_path_test_data);
+    }    
+    
+    [Benchmark]
+    [Arguments("test-data/192.txt")]
+    [Arguments("test-data/258.txt")]
+    [Arguments("test-data/519.1mb.csv")]
+    [Arguments("test-data/530.2mb.csv")]
+    [Arguments("test-data/545.5mb.csv")]
+    [Arguments("test-data/509.10mb.csv")]
+    [Arguments("test-data/558.50mb.csv")]
+    public
+        string
+                                        ReadAllTextWithFileUsingStreamRecyclableAndMemoryStreamAndStreamReaderAndReadBlockIntoZString_Direct
+                                        (
+                                            string file_path_test_data
+                                        )
+    {
+        return Core.IO.File.ReadAllTextWithFileUsingStreamRecyclableAndMemoryStreamAndStreamReaderAndReadBlockIntoZString
+                                                                                (
+                                                                                    file_path_test_data
+                                                                                );
     }    
 }
