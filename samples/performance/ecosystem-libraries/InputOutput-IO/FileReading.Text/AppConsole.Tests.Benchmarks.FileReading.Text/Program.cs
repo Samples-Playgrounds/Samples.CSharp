49d2982f31b1f2cd9b16b73092d29663b1cfb360
moljac <mcvjetko@holisticware.net>
2025-03-18 22:57:57 +0100

    Update Program.cs

--- samples/performance/ecosystem-libraries/InputOutput-IO/FileReading.Text/AppConsole.Tests.Benchmarks.FileReading.Text/Program.cs
+++ samples/performance/ecosystem-libraries/InputOutput-IO/FileReading.Text/AppConsole.Tests.Benchmarks.FileReading.Text/Program.cs
@@ -5,4 +5,11 @@ using Holisticware.Library.Snippets.ControlStructures.RangeMatching;
 
 Summary summary = BenchmarkRunner.Run<Benchmarks_FileReading_Text>();
 
+string content = string.Empty;
+
+Core.IO.File.ReadAllTextImplementation 
+        = Core.IO.File.ReadAllTextWithFileUsingStreamRecyclableAndMemoryStreamAndStreamReaderAndReadBlockIntoZString;
+
+content = Core.IO.File.ReadAllText("test-data/192.txt");
+
 return;
