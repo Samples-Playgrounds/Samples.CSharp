52e345e3fc1904355333814e5996e68e7f82be4b
moljac <mcvjetko@holisticware.net>
2025-06-01 16:04:05 +0200

    Update Program.cs

--- samples/performance/ecosystem-libraries/Formatters-Serialization-Deserialization/Textual-HumanReadable/XML/AppConsole.Tests.Benchmarks.XML/Program.cs
+++ samples/performance/ecosystem-libraries/Formatters-Serialization-Deserialization/Textual-HumanReadable/XML/AppConsole.Tests.Benchmarks.XML/Program.cs
@@ -3,16 +3,19 @@ using BenchmarkDotNet.Running;
 
 using Holisticware.Library.Snippets.XML;
 
-Summary summary_w = BenchmarkRunner.Run<Benchmarks_XML_Weather>();
-Summary summary_i = BenchmarkRunner.Run<Benchmarks_XML_Iris>();
+//Summary summary_w = BenchmarkRunner.Run<Benchmarks_XML_Weather>();
+//Summary summary_i = BenchmarkRunner.Run<Benchmarks_XML_Iris>();
+
+Benchmarks_XML_Weather bm = new();
+
+string content = string.Empty;
 
 /*
-Benchmarks_XML bm = new();
-
-   string content = string.Empty;
-
-content = bm.Test_12_System_Xml_Serialization_Serialize_with_MemoryStream_Iris();
-content = bm.Test_12_System_Xml_Serialization_Serialize_with_RecyclableMemoryStream_Iris();
 */
+
+content = bm.Test_11_Serialize_01_System_Runtime_Serialization_Serialize_with_MemoryStream();
+content = bm.Test_12_Serialize_01_System_Xml_Serialization_Serialize_with_MemoryStream();
+
+bm.Test_21_Deserialize_02_System_Runtime_Serialization_with_RecyclableMemoryStream();
 
 return;
