09868dfa4e7f2ce635f2f7de19a2661020f1ef05
moljac <mcvjetko@holisticware.net>
2025-06-01 16:04:03 +0200

    Create Weather.sxs.cs

---
+++ samples/performance/ecosystem-libraries/Formatters-Serialization-Deserialization/Textual-HumanReadable/XML/Holisticware.Library.Snippets.XML/Models/Weather.sxs.cs
@@ -0,0 +1,11 @@
+namespace Holisticware.Library.Snippets.XML.Models;
+
+[
+    System.Xml.Serialization.XmlRoot
+                                (
+                                )
+]
+public partial class
+                                        Weather
+{
+}
