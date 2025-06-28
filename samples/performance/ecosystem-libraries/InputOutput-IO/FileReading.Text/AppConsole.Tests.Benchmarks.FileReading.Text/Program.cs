c0f16f0168d409eb01a19a4b22121bbb9614a4f6
moljac <mcvjetko@holisticware.net>
2025-06-28 07:53:10 +0200

    improvements

--- samples/performance/ecosystem-libraries/InputOutput-IO/FileReading.Text/AppConsole.Tests.Benchmarks.FileReading.Text/Program.cs
+++ samples/performance/ecosystem-libraries/InputOutput-IO/FileReading.Text/AppConsole.Tests.Benchmarks.FileReading.Text/Program.cs
@@ -1,17 +1,30 @@
-﻿using BenchmarkDotNet.Reports;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
 
 using Holisticware.Library.Snippets.FileReading.Text;
 
-Summary summary_read_alltext = BenchmarkRunner.Run<Benchmarks_FileReading_Text_ReadAllText>();
-Summary summary_read_alllines = BenchmarkRunner.Run<Benchmarks_FileReading_Text_ReadAllLines>();
+string timestamp = DateTime.Now.ToString("yyyy-MM-dd-T-HH-mm-ss");
+string path = $"./BenchmarkDotNet.Artifacts/{timestamp}/";
 
-/*
+Summary summary_read_alltext = BenchmarkRunner.Run<Benchmarks_FileReading_Text_ReadAllText>
+                                                    (
+                                                        DefaultConfig.Instance
+                                                                        // relative from csproj project file
+                                                                        .WithArtifactsPath(path)
+                                                    );
+Summary summary_read_alllines = BenchmarkRunner.Run<Benchmarks_FileReading_Text_ReadAllLines>
+                                                    (
+                                                        DefaultConfig.Instance
+                                                                        // relative from csproj project file
+                                                                        .WithArtifactsPath(path)
+                                                    );
+
+
 string content = string.Empty;
 Core.IO.File.ReadAllTextImplementation
         = Core.IO.File.ReadAllTextWithFileUsingStreamRecyclableAndMemoryStreamAndStreamReaderAndReadBlockIntoZString;
 
-content = Core.IO.File.ReadAllText("test-data/steps-01/kb.1.txt");
-*/
+content = Core.IO.File.ReadAllText("td/s1/kb.1.txt");
 
 return;
