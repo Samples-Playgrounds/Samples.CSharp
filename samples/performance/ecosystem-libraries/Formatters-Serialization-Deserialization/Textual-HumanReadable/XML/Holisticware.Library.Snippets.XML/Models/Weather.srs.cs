3660b8cc1181f113e706a6709db09fbc4d94368a
moljac <mcvjetko@holisticware.net>
2025-10-14 18:18:28 +0200

    Update Weather.srs.cs

--- samples/performance/ecosystem-libraries/Formatters-Serialization-Deserialization/Textual-HumanReadable/XML/Holisticware.Library.Snippets.XML/Models/Weather.srs.cs
+++ samples/performance/ecosystem-libraries/Formatters-Serialization-Deserialization/Textual-HumanReadable/XML/Holisticware.Library.Snippets.XML/Models/Weather.srs.cs
@@ -1,10 +1,12 @@
 namespace Holisticware.Library.Snippets.XML.Models;
 
+/*
 [
     System.Runtime.Serialization.DataContract
     (
     )
 ]
+*/
 public partial class
                                         Weather
 {
