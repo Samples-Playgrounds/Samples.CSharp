26214b8894e275970db475084c8297eedb6a70c5
moljac <mcvjetko@holisticware.net>
2025-10-16 15:47:01 +0200

    Create Core.Data.Formatters.Text.Utilities.StreamExtensions.cs

---
+++ samples/performance/ecosystem-libraries/Formatters-Serialization-Deserialization/Textual-HumanReadable/XML/Holisticware.Library.Snippets.XML/Core.Data.Formatters/Text/XML/Core.Data.Formatters.Text.Utilities.StreamExtensions.cs
@@ -0,0 +1,77 @@
+namespace Core.Data.Formatters.Text.Utilities;
+
+/// <summary>
+/// 
+/// </summary>
+public static partial class
+                                        StreamExtensions
+{
+    private static readonly 
+        Microsoft.IO.RecyclableMemoryStreamManager
+                                        manager
+                                        = new ();
+    
+        public static
+        string
+                                        StringFromStream
+                                        (
+                                            this global::System.IO.Stream s
+                                        )
+    {
+        return StringFromStream(s, global::System.Text.Encoding.Unicode);
+    }
+    
+
+    public static
+        string
+                                        StringFromStream
+                                        (
+                                            this global::System.IO.Stream s,
+                                            global::System.Text.Encoding e
+                                        )
+    {
+        string? result = default(string);
+        
+        s.Position = 0;
+        using (TextReader r = new StreamReader(s, e))
+        {
+            result = r.ReadToEnd();
+        }
+
+        return result;
+    }
+    
+    public static
+        Microsoft.IO.RecyclableMemoryStream
+                                        ToRecyclableMemoryStream
+                                        (
+                                            this string str
+                                        )
+
+    {
+        Microsoft.IO.RecyclableMemoryStream ms = manager.GetStream();
+        global::System.IO.StreamWriter w = new (ms);
+        w.Write(str);
+        w.Flush();
+        ms.Position = 0;
+        
+        return ms;
+    }
+    
+    public static
+        global::System.IO.MemoryStream
+                                        ToMemoryStream
+                                        (
+                                            this string str
+                                        )
+
+    {
+        global::System.IO.MemoryStream ms = new ();
+        global::System.IO.StreamWriter w = new (ms);
+        w.Write(str);
+        w.Flush();
+        ms.Position = 0;
+        
+        return ms;
+    }
+}
