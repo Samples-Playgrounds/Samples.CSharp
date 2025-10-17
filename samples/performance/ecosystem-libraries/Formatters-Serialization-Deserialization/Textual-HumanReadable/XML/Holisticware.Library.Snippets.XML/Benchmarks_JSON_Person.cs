6956678705c2b1d027c5e3932053859c657cc6e4
moljac <mcvjetko@holisticware.net>
2025-10-18 00:03:53 +0200

    Update Benchmarks_JSON_Person.cs

--- samples/performance/ecosystem-libraries/Formatters-Serialization-Deserialization/Textual-HumanReadable/XML/Holisticware.Library.Snippets.XML/Benchmarks_JSON_Person.cs
+++ samples/performance/ecosystem-libraries/Formatters-Serialization-Deserialization/Textual-HumanReadable/XML/Holisticware.Library.Snippets.XML/Benchmarks_JSON_Person.cs
@@ -8,6 +8,7 @@ using FormatterNewtonsoftJSONNET=Core.Data.Formatters.Text.JSON.Newtonsoft.JSON.
 using FormatterSpanJSON=Core.Data.Formatters.Text.JSON.SpanJSON.Formatter;
 using FormatterNetJSON=Core.Data.Formatters.Text.JSON.NetJSON.Formatter;
 using FormatterSerdeNET=Core.Data.Formatters.Text.JSON.Serde.NET.Formatter;
+using FormatterUtf8Json=Core.Data.Formatters.Text.JSON.Utf8Json.Formatter;
 
 namespace Holisticware.Library.Snippets.JSON;
 
@@ -84,14 +85,18 @@ public partial class
         string
                                         json_people =
                                                     """
-                                                    
+                                                    [
+                                                        {"Name":"Jon Doe","Age":30,"City":"New York"},
+                                                        {"Name":"Jane Smith","Age":28,"City":"Los Angeles"},
+                                                        {"Name":"Bob Wilson","Age":42,"City":"Chicago"}
+                                                    ]
                                                     """;
 
     public static readonly
         string
                                         json_person =
                                                     """
-                                                    
+                                                    {"Name":"Jon Doe","Age":30,"City":"New York"}
                                                     """;
                 
     [Benchmark]
@@ -104,16 +109,139 @@ public partial class
         return FormatterSystemTextJson.Serialize(obj_person);
     }
     
-    /*
+
     [Benchmark]
     public
         Person?
-                                        Test_01_System_Text_Json_01_Deserialize
+                                        Test_01_System_Text_Json_02_Deserialize
                                         (
                                         )
     {
         return FormatterSystemTextJson.Deserialize<Person>(json_person);
     }
+
+    
+    
+    
+    [Benchmark]
+    public
+        string?
+                                        Test_02_Newtonsoft_JSON_NET_01_Serialize
+                                        (
+                                        )
+    {
+        return FormatterNewtonsoftJSONNET.Serialize(obj_person);
+    }
+    
+
+    [Benchmark]
+    public
+        string?
+                                        Test_02_Newtonsoft_JSON_NET_01_Serialize_People
+                                        (
+                                        )
+    {
+        return FormatterNewtonsoftJSONNET.Serialize(obj_people);
+    }
+    
+    [Benchmark]
+    public
+        Person?
+                                        Test_02_Newtonsoft_JSON_NET_02_Deserialize
+                                        (
+                                        )
+    {
+        return FormatterNewtonsoftJSONNET.Deserialize<Person>(json_person);
+    }
+    
+    
+    [Benchmark]
+    public
+        string?
+                                        Test_03_SpanJSON_01_Serialize
+                                        (
+                                        )
+    {
+        return FormatterSpanJSON.Serialize(obj_person);
+    }
+    
+
+    [Benchmark]
+    public
+        Person?
+                                        Test_03_SpanJSON_02_Deserialize
+                                        (
+                                        )
+    {
+        return FormatterSpanJSON.Deserialize<Person>(json_person);
+    }
+    
+    
+    [Benchmark]
+    public
+        string?
+                                        Test_04_NetJSON_01_Serialize
+                                        (
+                                        )
+    {
+        return FormatterNetJSON.Serialize(obj_person);
+    }
+    
+
+    [Benchmark]
+    public
+        Person?
+                                        Test_04_NetJSON_02_Deserialize
+                                        (
+                                        )
+    {
+        return FormatterNetJSON.Deserialize<Person>(json_person);
+    }
+    
+    
+    /*
+    [Benchmark]
+    public
+        string?
+                                        Test_05_SerdeNET_01_Serialize
+                                        (
+                                        )
+    {
+        return FormatterSerdeNET.Serialize(obj_person);
+    }
+
+    [Benchmark]
+    public
+        Person?
+                                        Test_05_SerdeNET_02_Deserialize
+                                        (
+                                        )
+    {
+        return FormatterSerdeNET.Deserialize<Person>(json_person);
+    }
     */
+     
+    
+    [Benchmark]
+    public
+        string?
+                                        Test_06_Utf8Json_01_Serialize
+                                        (
+                                        )
+    {
+        return FormatterUtf8Json.Serialize(obj_person);
+    }
+    
+
+    [Benchmark]
+    public
+        Person?
+                                        Test_06_Utf8Json_02_Deserialize
+                                        (
+                                        )
+    {
+        return FormatterUtf8Json.Deserialize<Person>(json_person);
+    }
+   
 }
             
