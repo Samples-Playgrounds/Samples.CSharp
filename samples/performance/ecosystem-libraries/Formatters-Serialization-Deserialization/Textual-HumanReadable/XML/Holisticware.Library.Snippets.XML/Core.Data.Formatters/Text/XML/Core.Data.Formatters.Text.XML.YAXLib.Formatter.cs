75d94cb5f1ddc5ee0c6a13e95cc6f9ba82227a5d
moljac <mcvjetko@holisticware.net>
2025-10-20 10:17:17 +0200

    Update Core.Data.Formatters.Text.XML.YAXLib.Formatter.cs

--- samples/performance/ecosystem-libraries/Formatters-Serialization-Deserialization/Textual-HumanReadable/XML/Holisticware.Library.Snippets.XML/Core.Data.Formatters/Text/XML/Core.Data.Formatters.Text.XML.YAXLib.Formatter.cs
+++ samples/performance/ecosystem-libraries/Formatters-Serialization-Deserialization/Textual-HumanReadable/XML/Holisticware.Library.Snippets.XML/Core.Data.Formatters/Text/XML/Core.Data.Formatters.Text.XML.YAXLib.Formatter.cs
@@ -5,51 +5,38 @@ public partial class
 {
     public static
         string
-                                        SerializeNaive<T>
+                                        Serialize<T>
                                         (
                                             T t
                                         )
     {
         string? result = default(string);
         
-        return result;
-    }
-
-    public static
-        string
-                                        SerializeCached<T>
-                                        (
-                                            T t
-                                        )
-    {
-        string? result = default(string);
+        global::YAXLib.YAXSerializer serializer = new (typeof(T));
+        /*
+        StringWriter sw = new ();
+        global::System.Xml.XmlWriter xw = global::System.Xml.XmlWriter.Create(sw);
+        serializer.Serialize(t, xw);
+        xw.Flush();
+        result = sw.ToString();
+        */
+        result = serializer.Serialize(t);
         
         return result;
     }
     
     public static
         T
-                                        DeserializeNaive<T>
+                                        Deserialize<T>
                                         (
                                             string xml
                                         )
     {
         T? t = default(T);
         
-        return t;
-    }
-    
-    public static
-        T?
-                                        DeserializeCached<T>
-                                        (
-                                            string xml
-                                        )
-    {
-        T? t = default(T);
+        global::YAXLib.YAXSerializer deserializer = new (typeof(T));
+        t = (T) deserializer.Deserialize(xml);
         
         return t;
     }
-
-
 }
